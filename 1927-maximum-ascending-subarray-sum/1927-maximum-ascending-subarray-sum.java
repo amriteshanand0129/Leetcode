0f4a580cfc895d4e46847cfddab6df84c52d6ae5
AMRITESH ANAND <115371712+amriteshanand0129@users.noreply.github.com>
2025-02-05 00:03:56 +0530

    Time: 0 ms (100%), Space: 41.5 MB (29.14%) - LeetHub

---
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max_sum = nums[0], sum = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] <= nums[i-1]) 
+                sum = nums[i];
+            else {
+                sum += nums[i];
+                max_sum = Math.max(max_sum, sum);
+            }
+        }
+        return max_sum;
+    }
+}
