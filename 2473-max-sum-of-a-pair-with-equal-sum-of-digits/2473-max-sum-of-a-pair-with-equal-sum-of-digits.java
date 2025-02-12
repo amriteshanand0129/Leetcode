2d61444acfb41822a9a0dae287e42a9372804f2e
AMRITESH ANAND <115371712+amriteshanand0129@users.noreply.github.com>
2025-02-12 23:45:47 +0530

    Time: 57 ms (31.28%), Space: 55.8 MB (78.38%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -8,26 +8,28 @@ class Solution {
         return sum;
     }
     public int maximumSum(int[] nums) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        Arrays.sort(nums);
+
+        HashMap<Integer, int[]> map = new HashMap<>();
         for(int i=0; i<nums.length; i++) {
             int sum = sumOfDigits(nums[i]);
-            if(map.containsKey(sum))
-                map.get(sum).add(i); 
+            if(map.containsKey(sum)) {
+                int[] arr = map.get(sum);
+                if(arr[1] != -1) 
+                    arr[0] = arr[1];
+                arr[1] = i;
+            }
             else {
-                ArrayList<Integer> arr = new ArrayList<>();
-                arr.add(i);
+                int[] arr = new int[2];
+                arr[0] = i; arr[1] = -1;
                 map.put(sum, arr);
             }
         }
         int max_sum = -1;
         for(int sum: map.keySet()) {
-            if(map.get(sum).size() > 1) {
-                ArrayList<Integer> arr = map.get(sum);
-                for(int i=0; i<arr.size(); i++) {
-                    for(int j=i+1; j<arr.size(); j++) {
-                        max_sum = Math.max(max_sum, nums[arr.get(i)] + nums[arr.get(j)]);
-                    }
-                }
+            if(map.get(sum)[1] != -1) {
+                int[] arr = map.get(sum);
+                max_sum = Math.max(max_sum, nums[arr[0]] + nums[arr[1]]);
             }
         }
         return max_sum;
